52e909ee34bf9cb675360c78e458a16b3a3a64dd
Miroslava Guzman <nadiamrslv@gmail.com>
2020-12-21 15:26:42 -0600

    fin seccion 3- diseño

---
+++ src/app/pages/notpagefound/notpagefound.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notpagefound',
+  templateUrl: './notpagefound.component.html',
+  styleUrls: [ './nopagefound.component.css'
+  ]
+})
+export class NotpagefoundComponent  {
+
+year = new Date().getFullYear();
+
+}
